f0bb84f4ab3fb86dcaa42128927b8df37c96390d
Mathias Gagnepain <mathias.gagnepain@algosup.com>
2024-04-02 13:31:46 +0200

    adding gitignore

--- src/main.py
+++ src/main.py
@@ -15,7 +15,35 @@ if SendMessage( GetForegroundWindow(), WM_INPUTLANGCHANGEREQUEST, 0, 0x4260426) 
     print('Keyboard layout changed!')
 
 for layout in keyboardList:
-    if layout != 1033:  # 1033 is the decimal equivalent of 00000409
+    if layout != 1033:
         win32api.LoadKeyboardLayout(str(layout), 0) 
 
 print("All configured keyboard languages unloaded!")
+
+import winreg
+
+def remove_keyboard_layout(layout_id):
+    # Open the keyboard layouts key
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Keyboard Layout\\Preload", 0, winreg.KEY_ALL_ACCESS)
+
+    try:
+        i = 0
+        while True:
+            # Enumerate the values
+            value_name, value_data, _ = winreg.EnumValue(key, i)
+
+            # If the value data matches the layout ID, delete the value
+            if value_data == layout_id:
+                winreg.DeleteValue(key, value_name)
+                print(f"Removed keyboard layout {layout_id}")
+
+            i += 1
+    except WindowsError:
+        # No more values to enumerate
+        pass
+
+    # Close the key
+    winreg.CloseKey(key)
+
+# Remove the Latvian keyboard layout
+remove_keyboard_layout("00000409")
